00c8c3cbcc360b3c4ca8c4bda5a1a65afae54682
iNaoi122 <88289995+iNaoi122@users.noreply.github.com>
2023-06-08 14:55:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,132 @@
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import ForceReply, Update
+from setting import Token
+import json
+import pandas
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+def reg_user_in_db(user):
+    user_json = {
+        "name": user,
+        "КК": 0,
+        "ДК": 0,
+        "ТИ": 0,
+        "СИМ": 0,
+        "МНП": 0,
+        "ПД": 0,
+        "НВС": 0,
+        "МБ": 0,
+        "ТПРО": 0,
+        "ОЛЕГ": 0,
+        "ЗК": 0,
+    }
+    try:
+        with open('db_users.json', encoding="utf-8") as f:
+            data = json.load(f)
+    except:
+        data = []
+    if not user in data:
+        data.append(user_json)
+    else:
+        print(1)
+
+    with open('db_users.json', "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def update_user_in_db(user, message):
+    try:
+        with open("db_users.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+    except:
+        print("Ошибка чтения файла")
+        data = []
+
+    for item in data:
+        if item['name'] == user:
+            add_offers(item, message)
+
+    with open('db_users.json', "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def start_new_month():
+    with open("db_users.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    for item in data:
+        item['sales'] = 0
+
+    with open('db_users.json', "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def add_offers(item, message):
+    if "КК" in message:
+        item["КК"] += int(message[message.index("КК") + 1])
+    if "ДК" in message:
+        item["ДК"] += int(message[message.index("ДК") + 1])
+    if "ТИ" in message:
+        item["ТИ"] += int(message[message.index("ТИ") + 1])
+    if "СИМ" in message:
+        item["СИМ"] += int(message[message.index("СИМ") + 1])
+    if "МНП" in message:
+        item["МНП"] += int(message[message.index("МНП") + 1])
+    if "ПД" in message:
+        item["ПД"] += int(message[message.index("ПД") + 1])
+    if "НВС" in message:
+        item["НВС"] += int(message[message.index("НВС") + 1])
+    if "МБ" in message:
+        item["МБ"] += int(message[message.index("МБ") + 1])
+    if "ТПРО" in message:
+        item["ТПРО"] += int(message[message.index("ТПРО") + 1])
+    if "ОЛЕГ" in message:
+        item["ОЛЕГ"] += int(message[message.index("ОЛЕГ") + 1])
+    if "ЗК" in message:
+        item["ЗК"] += int(message[message.index("ЗК") + 1])
+
+
+def static():
+    with open("db_users.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+        print(data)
+
+
+async def update_sales(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    update_user_in_db(user.username, update.message.text.split())
+    print(update.message.text.split())
+
+
+async def reg(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.message.from_user
+
+    reg_user_in_db(user.username)
+    await update.message.reply_text(text="Привет, " + user.username + "! Введите ваше имя и количество продаж")
+
+
+async def static(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.message.from_user
+    with open("db_users.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    curr = list(filter(lambda x: x["name"] == user.username, data))
+    await update.message.reply_text(text=str(curr))
+
+
+def main() -> None:
+    app = Application.builder().token(Token).build()
+
+    app.add_handler(CommandHandler("start", reg))
+    app.add_handler(CommandHandler("send", update_sales))
+    app.add_handler(CommandHandler('static', static))
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(start_new_month, 'cron', day='1')
+    scheduler.start()
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()

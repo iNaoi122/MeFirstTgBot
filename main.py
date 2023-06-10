a096310fd60a026b0014652fc8ddbad73a172e48
iNaoi122 <88289995+iNaoi122@users.noreply.github.com>
2023-06-10 22:25:33 +0300

    Update main.py
    Вывод оформлен красиво.

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
-from telegram import ForceReply, Update
+from telegram import Update
 from setting import Token
 import json
-import pandas
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
 def reg_user_in_db(user):
     user_json = {
         "name": user,
+        "ВС": 0,
+        "НВС": 0,
         "КК": 0,
         "ДК": 0,
         "ТИ": 0,
+        "МБ": 0,
         "СИМ": 0,
         "МНП": 0,
         "ПД": 0,
-        "НВС": 0,
-        "МБ": 0,
         "ТПРО": 0,
         "ОЛЕГ": 0,
         "ЗК": 0,
@@ -63,6 +63,8 @@ def start_new_month():
 
 
 def add_offers(item, message):
+    if "ВС" in message:
+        item['ВС'] += int(message[message.index("ВС") + 1])
     if "КК" in message:
         item["КК"] += int(message[message.index("КК") + 1])
     if "ДК" in message:
@@ -97,30 +99,28 @@ async def update_sales(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     user = update.effective_user
     update_user_in_db(user.username, update.message.text.split())
     print(update.message.text.split())
+    with open("db_users.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    curr = list(filter(lambda x: x["name"] == user.username, data))
+    text = ''
+    for key, value in curr[0].items():
+        text += f'{key}: {value}\n'
+    await update.message.reply_text(text=text)
 
 
 async def reg(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.message.from_user
 
     reg_user_in_db(user.username)
-    await update.message.reply_text(text="Привет, " + user.username + "! Введите ваше имя и количество продаж")
-
-
-async def static(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.message.from_user
-    with open("db_users.json", "r", encoding="utf-8") as file:
-        data = json.load(file)
-
-    curr = list(filter(lambda x: x["name"] == user.username, data))
-    await update.message.reply_text(text=str(curr))
+    await update.message.reply_text(text="Привет, " + user.username + "! В  ведите ваше имя и количество продаж")
 
 
 def main() -> None:
     app = Application.builder().token(Token).build()
 
-    app.add_handler(CommandHandler("start", reg))
+    app.add_handler(CommandHandler("reg", reg))
     app.add_handler(CommandHandler("send", update_sales))
-    app.add_handler(CommandHandler('static', static))
     scheduler = BackgroundScheduler()
     scheduler.add_job(start_new_month, 'cron', day='1')
     scheduler.start()
@@ -129,4 +129,7 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except:
+        pass
